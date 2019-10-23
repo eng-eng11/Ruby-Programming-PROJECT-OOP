e708f39cea864bc292ffea18a45263e3fe7b65c4
davidauza.engineer <davidauza01@gmail.com>
2019-10-23 15:08:37 -0500

    Update.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,33 +1,39 @@
-# frozen_string_literal: true
-
-require './lib/interface'
-
 class Board
-  WIN_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-               [1, 4, 7], [2, 5, 8], [3, 6, 9],
-               [1, 5, 9], [3, 5, 7]].freeze
-
   include Interface
+
   attr_accessor :positions
 
-  def initialize
+  def initialize()
     @positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  end
-
-  def update(input, char)
-    @positions[input - 1] = char
+    @lines = [
+        [1,2,3],[4,5,6],[7,8,9],
+        [1,4,7],[2,5,8],[3,6,9],
+        [1,5,9],[3,5,7]
+    ]
     show(@positions)
   end
 
+
+  def update(input, char)
+    @positions[input-1] = char
+  end
+
   def full?
-    @positions.all? { |x| x.instance_of?(String) }
+    if @positions.all?{|x| x.instance_of?(String)}
+      get_input('full')
+      return true
+    end
   end
 
-  def win?(inputs)
-    WIN_LINES.any? { |x| x - inputs == [] }
+  def win?(player, board)
+    if @lines.any?{|x| x - player.inputs == []}
+      winner_display(player.name, board.positions)
+      return true
+    end
   end
 
-  def taken?(input, positions = @positions)
-    positions[input - 1].is_a? String
+  def taken?(input)
+    @positions[input-1] == "X" || @positions[input-1] == "O"
   end
+
 end
