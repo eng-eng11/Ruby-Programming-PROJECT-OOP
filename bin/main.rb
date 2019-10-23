693a8d36b40a885d7041a9d423514bccc15ab1ca
davidauza.engineer <davidauza01@gmail.com>
2019-10-23 14:57:10 -0500

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,102 +1,25 @@
-#!/usr/bin/env ruby
-# this method creates the board for the game
-def create_board
-  matrix = Array.new(9)
-  i = 0
-  while i < matrix.length
-    matrix[i] = '-'
-    i += 1
-  end
-  matrix
-end
+require 'colorize'  # Need to run 'gem install colorize'
 
-# this method checks if the selected cell is empty: Empty means containing a '-'
-def check_matrix_cell(cell_to_check)
-  if $board[cell_to_check - 1] == '-'
-    true
-  else
-    false
-  end
-end
+# self customized libary
+require './lib/interface'
+require './lib/board'
+require './lib/player'
+require './lib/game'
 
-# this method update the matrix according to the player movement
-def update_matrix(player_id, selected_cell)
-  if player_id == 1
-    figure = 'X'
-  else
-    figure = 'O'
-  end
-  $board[selected_cell - 1] = figure
-  print_board
-end
+include Interface
 
-# this method prints the board
-def print_board
-  i = 0
-  while i < $board.length
-    print $board[i] + ' '
-    if (i + 1) % 3 == 0
-      puts ' '
-    end
-    i += 1
-  end
-end
+loop do
+  show_text('welcome')
 
-# this method checks whether there is a winner or not after movement number 5
-def check_matrix_for_winner(player_id)
-  if player_id == 1
-    figure = 'X'
-  else
-    figure = 'O'
-  end
-  if $board[0] == figure && $board[3] == figure && $board[6] == figure ||
-      $board[1] == figure && $board[4] == figure && $board[7] == figure ||
-      $board[2] == figure && $board[5] == figure && $board[8] == figure ||
-      $board[0] == figure && $board[1] == figure && $board[2] == figure ||
-      $board[3] == figure && $board[4] == figure && $board[5] == figure ||
-      $board[6] == figure && $board[7] == figure && $board[8] == figure ||
-      $board[0] == figure && $board[4] == figure && $board[8] == figure ||
-      $board[2] == figure && $board[4] == figure && $board[6] == figure
-      true
-  else
-    false
-  end
-end
+  player1 = Player.new(get_input('player1'), "X".red)
+  player2 = Player.new(get_input('player2'), "O".green)
+  board = Board.new
 
-puts "Welcome, below you can find the board for the game:"
-$board = create_board
-print_board
-i = 0
-while i < $board.length
-  if (i % 2) == 0
-    player_id = 1
-  else
-    player_id = 2
+  game = Game.new(player1, player2, board)
+
+  if !game.play
+    show_text('finish')
+    break
   end
-  puts "Player #{player_id} please make your move:"
-  valid_input = false
-  while !valid_input
-    player_selected_cell = gets.chomp.to_i
-    if player_selected_cell > 0 && player_selected_cell < 10
-      if check_matrix_cell(player_selected_cell)
-        update_matrix(player_id, player_selected_cell)
-        valid_input = true
-      else
-        puts "Cell already selected, please select another one:"
-      end
-    else
-      puts "Please enter a number between 1 and 9:"
-    end
-  end
-  if i >= 4
-    if check_matrix_for_winner(player_id)
-      puts "\n\nGame Over!\n\nPlayer #{player_id} wins!"
-      break
-    else
-      if i == 8
-        puts "\n\nIt's a Draw!"
-      end
-    end
-  end
-  i += 1
+
 end
